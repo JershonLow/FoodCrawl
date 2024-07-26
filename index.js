4e12fdafe7ea29d85fa974ffbbb7d8fcdb1ebdf2
JershonLow <s4023120@student.rmit.edu.au>
2024-07-26 18:16:35 +1000

    Added image carousel

--- index.js
+++ index.js
@@ -73,10 +73,67 @@ function checkColour(coordinateStr, square) {
   let element = document.getElementById(coordinateStr);
 
   if (square.isChecked == true) {
-    element.style.backgroundColor = "#7693A6";
+    element.style.backgroundColor = "#CFE4EF";
     element.style.boxShadow = "0px 0px 5px rgba(137, 237, 226, 0.25)";
   } else {
     element.style.backgroundColor = "#fafeff";
     element.style.boxShadow = "2.5px 2.5px 5px rgba(0, 0, 0, 0.589)";
   }
+}
+
+// Get the popup elements
+var popup = document.getElementById("carouselPopup");
+var carouselImage = document.getElementById("carouselImage");
+var captionText = document.getElementById("caption");
+var currentIndex = 0;
+var images = [];
+
+// Get all restaurant divs
+var restaurants = document.querySelectorAll(".card-content");
+
+// Loop through all restaurants and add click event
+restaurants.forEach(function (restaurant) {
+  restaurant.addEventListener("click", function () {
+    images = JSON.parse(this.getAttribute("data-images"));
+    currentIndex = 0;
+    showImage(currentIndex);
+    popup.style.display = "block";
+  });
+});
+
+// Get the close button element
+var closeButton = document.getElementsByClassName("close")[0];
+
+// When the close button is clicked, hide the popup
+closeButton.addEventListener("click", function () {
+  popup.style.display = "none";
+});
+
+// Optional: Hide popup when clicking outside the carousel
+window.addEventListener("click", function (event) {
+  if (event.target === popup) {
+    popup.style.display = "none";
+  }
+});
+
+// Get the prev and next buttons
+var prevButton = document.querySelector(".prev");
+var nextButton = document.querySelector(".next");
+
+// When the prev button is clicked, show the previous image
+prevButton.addEventListener("click", function () {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  showImage(currentIndex);
+});
+
+// When the next button is clicked, show the next image
+nextButton.addEventListener("click", function () {
+  currentIndex = (currentIndex + 1) % images.length;
+  showImage(currentIndex);
+});
+
+// Function to show the image at the given index
+function showImage(index) {
+  carouselImage.src = "./Assets/Images/" + images[index];
+  captionText.innerHTML = `Image ${index + 1} of ${images.length}`;
 }
